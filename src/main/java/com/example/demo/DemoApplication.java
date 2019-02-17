ba37860bf3d61df1cf069b1679e7644303e935d8
Andrii K <kurazaev@gmail.com>
2019-02-17 13:12:01 +0200

    change visible message to "Test #1!"

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -17,6 +17,6 @@ public class DemoApplication {
     @RequestMapping(method = RequestMethod.GET,
         path = "/")
     public String index() {
-        return "Good Job, Andrii Kurazaiev!";
+        return "Test #1!";
     }
 }
