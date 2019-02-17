1cef05f37ba942849a8887882247842f0c3b53fd
Andrii K <kurazaev@gmail.com>
2019-02-17 13:12:18 +0200

    change visible message to "Test #1!"

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -17,6 +17,6 @@ public class DemoApplication {
     @RequestMapping(method = RequestMethod.GET,
         path = "/")
     public String index() {
-        return "Test #1!";
+        return "Test #1";
     }
 }
