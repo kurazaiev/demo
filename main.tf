cb5e2173b36aa8ce26eec47d0de1fd2f4eff679b
Andrii_Kurazaiev <kurazaev@gmail.com>
2019-02-27 17:27:21 +0200

    add build folder

--- main.tf
+++ main.tf
@@ -4,23 +4,37 @@ provider "google" {
   region = "europe-west1"
 }
 
-resource "google_container_cluster" "primary" {
+resource "google_container_cluster" "k8s-cluster" {
   name = "k8s-cluster"
   zone = "europe-west1-d"
   remove_default_node_pool = true
+  min_master_version = "1.11.7-gke.6"
   initial_node_count = 1
-  #logging_service = "logging.googleapis.com/kubernetes"
+  network = "${google_compute_network.k8s-network.name}"
+  subnetwork = "${google_compute_subnetwork.k8s-subnetwork.name}"
 }
 
-resource "google_container_node_pool" "primary_pool" {
-  name = "primary-pool"
-  cluster = "${google_container_cluster.primary.name}"
+resource "google_compute_network" "k8s-network" {
+  name = "k8s-network"
+  auto_create_subnetworks = "false"
+}
+
+resource "google_compute_subnetwork" "k8s-subnetwork" {
+  name = "k8s-subnetwork"
+  ip_cidr_range = "10.10.10.0/24"
+  network = "k8s-network"
+  region = "europe-west1"
+}
+
+resource "google_container_node_pool" "k8s-pool" {
+  name = "k8s-pool"
+  cluster = "${google_container_cluster.k8s-cluster.name}"
   zone = "europe-west1-d"
   version = "1.11.7-gke.6"
   node_count = "2"
 
+  machine_type = "n1-standard-1"
   node_config {
-    machine_type = "n1-standard-1"
     image_type = "GCI"
     disk_size_gb = 20
   }
