55fde483032c49af586e7c8344ecce2400f4808c
Andrii_Kurazaiev <kurazaev@gmail.com>
2019-02-28 14:52:41 +0200

    add build folder

--- main.tf
+++ main.tf
@@ -32,10 +32,10 @@ resource "google_container_node_pool" "k8s-pool" {
   zone = "europe-west1-d"
   version = "1.11.7-gke.6"
   node_count = "2"
-  machine_type = "n1-standard-1"
   node_config {
     image_type = "GCI"
     disk_size_gb = 20
+    machine_type = "n1-standard-1"
   }
   management {
     auto_repair = true
