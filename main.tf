f789cd374d36aeaf1e98034a697e4add9cd1ae65
Andrii_Kurazaiev <kurazaev@gmail.com>
2019-02-28 14:57:35 +0200

    add build folder

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = "${file("credentials.json")}"
+  credentials = "${file("./creds/credentials.json")}"
   project = "firstproject-230011"
   region = "europe-west1"
 }
